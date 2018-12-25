062e9bb9d02e842e95f41c88a8243b8db2828e70
cagataykula <36194319+cagataykula@users.noreply.github.com>
2018-12-25 16:44:12 +0300

    post detayı için servis bağlantısı

--- src/services/service.js
+++ src/services/service.js
@@ -3,6 +3,10 @@ export default {
         return firebase.database().ref('/posts').once('value');
     },
 
+    fetchPostDetail(postId) {
+        return firebase.database().ref('/posts/' + postId).once('value');
+    },
+
     changeLike(post){
         return firebase.database().ref('/posts/' + post.index + '/isLiked').set(!post.isLiked);
     }
