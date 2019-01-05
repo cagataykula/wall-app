b89ce7fa9368877cfec435dc6ac6bba168c9cf0c
Çağatay Kula <36194319+cagataykula@users.noreply.github.com>
2019-01-06 01:41:54 +0300

    Walldaki postlar için animasyon

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,16 +1,18 @@
 <template>
-    <div class="card">
-    <!-- <div class="card-header">Featured</div> -->
-    <div class="card-body" @click="routerLink">
-        <!-- <router-link :to="routerLink"> -->
-            <h5 class="card-title">{{this.post.title}}</h5>
-            <p class="card-text">{{this.post.body}}</p>
-        <!-- </router-link> -->
-    </div>
-    <div class="card-footer">
-        <like-button :post="this.post" :index="compIndex" :fromSinglePage="fromSinglePage" />
-    </div>
-    </div>
+    <transition :name="animation" appear>
+        <div class="card" v-if="show">
+        <!-- <div class="card-header">Featured</div> -->
+        <div class="card-body" @click="routerLink">
+            <!-- <router-link :to="routerLink"> -->
+                <h5 class="card-title">{{this.post.title}}</h5>
+                <p class="card-text">{{this.post.body}}</p>
+            <!-- </router-link> -->
+        </div>
+        <div class="card-footer">
+            <like-button :post="this.post" :index="compIndex" :fromSinglePage="fromSinglePage" />
+        </div>
+        </div>
+    </transition>
 </template>
 
 <script>
@@ -27,12 +29,20 @@ export default {
             required: true
         },
         index: {
-            type: String,
+            type: Number,
             required: true
         },
         fromSinglePage: {
             type: Boolean,
             required: false
+        },
+        show: {
+            type: Boolean,
+            required: true
+        },
+        animation: {
+            type: String,
+            required: true
         }
     },
     methods: {
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,22 @@
 <template>
     <section>
     <Header text="Gönderiler" />
+    <div class="row">
+        <div class="col-sm-9"></div>
+        <div class="col-sm-3">
+            <select v-model="animation">
+                <option value="slide-fade">Slide Fade</option>
+                <option value="fade">Fade</option>
+                <option value="blur">Blur</option>
+                <option value="bounce">Bounce</option>
+            </select>
+            <br>
+            <button @click="show = !show">{{ (show) ? "Gizle" : "Göster"  }}</button>
+        </div>
+    </div>
     <div class="container">
         <Loader v-if="isLoading" />
-        <Posts v-for="(post, index) in posts" :post="post" :index="index" :key="post.id" />
+        <Posts v-for="(post, index) in posts" :post="post" :index="index" :key="post.id" :show="show" :animation="animation" />
     </div>
     </section>
 </template>
@@ -17,7 +30,9 @@ export default {
     name: 'Home',
     data(){
         return {
-            isLoading: true
+            isLoading: true,
+            show: true,
+            animation: "blur"
         }
     },
     computed: {
@@ -36,3 +51,50 @@ export default {
 }
 </script>
 
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.blur-enter-active, .blur-leave-active {
+  transition: .5s;
+  filter: blur(0px);
+}
+.blur-enter, .blur-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  filter: blur(50px);
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+</style>
